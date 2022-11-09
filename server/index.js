02302d889e532b21bcb89c0eba16e9b18d855bea
nhasty <nyall.hasty@gmail.com>
2022-11-09 12:12:23 -0800

    added morgan logger

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const fs = require('fs');
 const router = require('./router');
 require('dotenv').config();
 
+// basic server setup
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/fec2/hr-rfp', router);
+
+// logger
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' });
+app.use(morgan('tiny', { stream: accessLogStream }));
+
+// loaderio testing
 app.use('/loaderio-92bb7174e20fe1de1142814ca2b76e78.txt', express.static(path.join(__dirname, '../server/loaderio.txt')));
 
+// router
+app.use('/api/fec2/hr-rfp', router);
 app.listen(process.env.PORT || 3000, (err) => {
   if (err) {
     console.log(err);
